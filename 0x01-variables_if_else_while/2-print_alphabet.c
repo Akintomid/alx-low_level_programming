5248191da4bd68df29aa3bd17d56c7a9379611d0
Akintomid <richardakintomide22@gmail.com>
2023-02-17 20:57:23 -0800

    variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main _ prints the alphabet in lowercase,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
