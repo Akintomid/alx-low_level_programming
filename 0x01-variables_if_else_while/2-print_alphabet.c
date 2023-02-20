9d0369ccc09ede60ff301ff947b06c4bfdcc62d8
Akintomid <richardakintomide22@gmail.com>
2023-02-20 02:11:55 -0800

    alphabt

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main _ prints the alphabet in lowercase,
- * followed by a new line
+ * main _ prints the alphabet in lowercase, followed by a new line
+ *
  * Return: Always 0 (success)
  */
 int main(void)
