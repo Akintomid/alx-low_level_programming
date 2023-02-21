ad0832e1b9c0b352cd9dbb449bec5ce6c72bafea
Akintomid <richardakintomide22@gmail.com>
2023-02-21 08:17:19 -0800

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * print_alphabet - Entry point.
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+	{
+		putchar (x);
+	}
+	putchar ('\n');
+}
