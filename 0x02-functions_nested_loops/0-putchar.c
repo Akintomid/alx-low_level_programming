90795fb873ec9356dbeac716ecabd76e0adc8b46
Akintomid <richardakintomide22@gmail.com>
2023-02-21 08:05:40 -0800

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,12 @@
-#include <stdio.h>
 #include "main.h"
+#include <stdio.h>
 /**
- * main - prints _putchar, followed by a new line
+ * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0.
  */
 int main(void)
 {
-	printf("_putchar");
+	printf("putchar");
 	return (0);
 }
