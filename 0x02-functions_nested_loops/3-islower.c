1e1d15b7caefab096e0f2b88d460533aa352f1d7
Akintomid <richardakintomide22@gmail.com>
2023-02-22 17:10:05 -0800

    abs

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 #include "main.h"
 /**
  * _islower - checks for lowercase character.
+ * @c: lowercase character.
  * Return: 1 if 'c' is lower case, 0 otherwise.
  */
 int _islower(int c)
--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * _isalpha - checks for alphabetic character
+ * _isalpha - checks for alphabetic character.
+ * @c: lowercase or uppercase character.
  * Return: 1 if 'c' is a letter, lowercase or uppercse, 0 otherwise.
  */
 int _isalpha(int c)
