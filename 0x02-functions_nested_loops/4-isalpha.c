b92103d551b049d5dafa4aad77e93e336d4a1ad4
Akintomid <richardakintomide22@gmail.com>
2023-02-22 16:50:35 -0800

    is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * int _isalpha - checks for alphabetic character
- * Return: 1 if c is a letter, lowercase or uppercse
- * return 0 otherwise.
+ * _isalpha - checks for alphabetic character
+ * Return: 1 if 'c' is a letter, lowercase or uppercse, 0 otherwise.
  */
 int _isalpha(int c)
 {
-	if ((( c >= 'a') && ( c <= 'z')) || (( c >= 'A') && ( c <= 'Z')))
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
 		return (1);
 	else
 		return (0);
