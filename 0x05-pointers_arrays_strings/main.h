5c28a96dccdf7237d59910f57223b703ca489cda
Akintomid <richardakintomide22@gmail.com>
2023-02-27 09:10:25 -0800

    0-6 pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef main_h
+#define main_h
+int _putchar(int c);
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+
+#endif
