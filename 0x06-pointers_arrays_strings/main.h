84e3166523c827f37201d3a50a8ede31be639c5f
Akintomid <richardakintomide22@gmail.com>
2023-03-01 06:44:38 -0800

    1-7

---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef main_h
+#define main_h
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+void reverse_array(int *a, int n);
+char *string_toupper(char *);
+char *cap_string(char *);
+char *leet(char *);
+
+#endif
