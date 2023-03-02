79a01220d455a2448ba49d2a7e22b5e09921531b
Akintomid <richardakintomide22@gmail.com>
2023-03-02 02:38:51 -0800

    101 print number

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,9 +3,7 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-void reverse_array(int *a, int n);
-char *string_toupper(char *);
-char *cap_string(char *);
-char *leet(char *);
+void _putchar(char c);
+void print_number(int n);
 
 #endif
