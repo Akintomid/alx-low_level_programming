a629c3930e648d5cf8c35d61bacb6945a552f032
Akintomid <richardakintomide22@gmail.com>
2023-03-03 08:42:16 -0800

    100 rot 13

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,7 +3,5 @@
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
-void _putchar(char c);
 void print_number(int n);
-
 #endif
