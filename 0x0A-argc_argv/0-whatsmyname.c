727a30437c07eaebc0fc4705dfdc26f0903a3be3
Akintomid <richardakintomide22@gmail.com>
2023-03-10 09:16:04 -0800

    name

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  * Return: Always 0 (success)
  */
 
-int main(int argc, char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
-	printf("%d\n", argc);
 	printf("%s", argv[0]);
 	return (0);
 }
--- 0x0A-argc_argv/main.h
+++ 0x0A-argc_argv/main.h
@@ -2,5 +2,6 @@
 #define main_h
 
 int _putchar(char c);
+int main(int argc __attribute__((unused)), char *argv[]);
 
 #endif
