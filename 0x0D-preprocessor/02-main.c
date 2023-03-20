dbc12118efda3c59e5c46176f97b3443d5b2f901
Akintomid <richardakintomide22@gmail.com>
2023-03-20 06:00:21 -0700

    File name

---
+++ 0x0D-preprocessor/02-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "0-object_like_macro.h"
+#include "1-pi.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	printf("Name of source file %s", __FILE__);
+	printf("\nName of source file %s", __FILE__);
+
+	return (0);
+}
