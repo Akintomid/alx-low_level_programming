08dda9fce5e05ab230bf3fd26885e86af583a766
Akintomid <richardakintomide22@gmail.com>
2023-04-09 03:41:58 -0700

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,52 +1,54 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
 #include "main.h"
 
 /**
- * read_textfile - reads and prints a text file
- * @filename: file name
- * @letters: number of letters to read and print
- * Return: the actual number of letters it could read and print
+ * read_textfile - function reads a text file and prints it
+ * @filename: name of file
+ * @letters: number of letters it should read and print
+ * Return: thr actual numbers of letters it could read and print
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	ssize_t r_count = fread(buffer, sizeof(char), letters, fl);
-	char *buff = (char *)malloc(letters + 1);
-	FILE *fl = fopen(filename, "r");
+	int fl = open(filename, O_RDONLY);
+	char *buffer = malloc(sizeof(char) * letters);
+	ssize_t b_read = read(fl, buffer, letters);
+	ssize_t b_written = write(STDOUT_FILENO, buffer, b_read);
 
 	if (filename == NULL)
-		return (0);
-
-	FILE *file = fopen(filename, "r");
-	if (file == NULL)
-		return (0);
-
-	if (buff == NULL)
 	{
-		fclose(file);
 		return (0);
 	}
 
-	if (r_count == -1)
+	if (fl == -1)
 	{
-		fclose(file);
-		free(buff);
 		return (0);
 	}
 
-	buff[r_count] = '\0';
-
-	if (fwrite(buff, sizeof(char), r_count, stdout) != r_count)
+	if (buffer == NULL)
 	{
-		fclose(file);
-		free(buff);
+		close(fl);
 		return (0);
 	}
 
-	fclose(file);
-	free(buff);
+	if (b_read == -1)
+	{
+		free(buffer);
+		close(fl);
+		return (0);
+	}
 
-	return (r_count);
+	if (b_written == -1 || b_written != b_read)
+	{
+		free(buffer);
+		close(fl);
+		return (0);
+	}
+
+	free(buffer);
+	close(fl);
+	return (b_written);
 }
