510239ddf2fcc2efae84a5d2f885a9955f27732f
Akintomid <richardakintomide22@gmail.com>
2023-11-02 02:29:25 -0700

    Technical interview preparation

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
+
+def island_perimeter(grid):
+    """Return the perimiter of an island.
+
+    The grid represents water by 0 and land by 1.
+
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    width = len(grid[0])
+    height = len(grid)
+    edges = 0
+    size = 0
+
+    for a in range(height):
+        for b in range(width):
+            if grid[a][b] == 1:
+                size += 1
+                if (b > 0 and grid[a][b - 1] == 1):
+                    edges += 1
+                if (a > 0 and grid[a - 1][b] == 1):
+                    edges += 1
+    return size * 4 - edges * 2
